65ae84831b00987c914f35d85be57f4814eeaac7
Liad Kashanovsky <liad@veeca.net>
2017-11-30 11:46:46 +0200

    Tasks List Ember + Laravel App Test: https://github.com/Kasha/Tasks.git Laravel Backend App: https://github.com/Kasha/Tasks-Laravel-Backend.git AppWithConsoleAndDB Demo https://youtu.be/0MdVnllu9ec Ember+Laravel Workspace https://youtu.be/7yRlZDx2uAc
    Issues:
    1. Using model.item and model.@attribute to trigger and update summary date with each change
    2. Adapter contains host attribute with Laravel API URL: http://localhost:8000/api'
    3. Overriding of BuildURL method in  Application Adapter and modifying  server requests  to work with Laravel Listener, instead of sending the Task ID as URI (additional route directory) ID is sent using query string id
    Previous BuildURL URL:
    http://localhost:8000/api/tasks/1
    Fixed URL:
    http://localhost:8000/api/tasks?id=1
    Read more:
    https://www.emberjs.com/api/ember-data/2.15/classes/DS.JSONAPIAdapter
    4. Task Title, Add and Update emphasizes
    a. Update:
    i. Mouse Double clicking on task, sets it to Edit mode allowing changing task title
    ii. Enter click for saving title text changes
    b. New Task:
    i. I’ve set text validation for Latin + Hebrew + Any valid name letter
    ii. Unless there is a valid text, clicking on Add new Task (enter on textbox or mouse click on the +) will display a message saying title text is not valid
    c.
    5. Database and migration:
    Database Provider: sqlite
    I’ve created Database using sqlite GUI studio - Database = Tasks
    And Tasks Table using migration as defined - Table = Tasks
    
    For migration to work I had to set the following in .env file in audition to database.php config file:
    
    DB_CONNECTION=sqlite
    DB_DATABASE=C:/Dev/Buyme/tasks/tasks.db
    
    Migration Table which was used to generate Tasks Table using migrate shell command (php artisan migrate)
    
    I used QueryBuilder for Database queries
    
    6. 2 Screen Videos demonstrating My developed Tasks App and Workspace with code
    AppWithConsoleAndDB Demo
    https://youtu.be/0MdVnllu9ec
    
    Ember Task List App running on localhost:4200 and calling Laravel Back end http://localhost:8000/api/tasks requests.
    You can see functionality as defined + Ember Inspector + Database
    
    Ember+Laravel Workspace
    https://youtu.be/7yRlZDx2uAc
    
    Visual Studio Code Workspace for Ember Front end and Laravel Backend, development and configuration files

---
+++ storage/framework/views/566f0334e45ea813846a50a7ead9927443805d37.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+    Tasks
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('tasksLayout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/b24eab9ccdb94989d839210c4a1586de250266bd.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1>Laravel Quickstart</h1>
+
+        <?php echo $__env->yieldContent('content'); ?>
+    </body>
+</html>
